ad16bc38cc5b4f4ceefbc3952c2e9871d5519fa1
CancuiW <can.cui@wayne.edu>
2023-06-30 11:31:29 -0700

    practice

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,7 +11,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React,{useState}from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -26,6 +26,8 @@ export const listOfAwesome = [
 ];
 
 export default function Programmers() {
+  const [pro,setPro]=useState(listOfAwesome)
+  const [feature,setFeature]=useState(null)
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
@@ -35,12 +37,21 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    //第一种方法
+    // for (let i=0;i<pro.length;i++){
+    //   if (pro[i].id===feature){
+    //     return listOfAwesome[i].name
+    //   }
+    // }
+    //第二种方法
+    const found=pro.find(x=>x.id===feature)
+    return found.name
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: feature ? "gold":'royalblue'// 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -48,12 +59,10 @@ export default function Programmers() {
       <h2>Programmers</h2>
       <div className='programmers'>
         {
-          /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
-          We might think: "it works, though!" But if the list of programmers is not state,
-          we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          
+          pro.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => {setFeature(dev.id)}}>Feature</button>
             </div>
           )
         }
@@ -63,7 +72,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          feature
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
